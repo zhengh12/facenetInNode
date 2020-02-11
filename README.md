e8cadce076338c0dec8ed391900a6a6cf1899b24
Your Name <you@example.com>
2020-02-11 18:12:28 +0800

    Signed-off-by: 2020.02.11

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Most of code in ./public/javascripts
 2020.01.16 finished alignFace.js and tested the result. 
 2020.02.10 add SVMClassifier.js to test SVMClassifier and use process to show load process 
+2020.02.11 SVMClassifier.js fineturn the factor of SVM.change different style of process bar
--- public/javascripts/SVMClassifier.js
+++ public/javascripts/SVMClassifier.js
@@ -1,10 +1,63 @@
 const tf = require("@tensorflow/tfjs-node");
-const loadImages = require("./cTreeClassifier");
 const facenet = require("./facenet")
 const detectFace = require("./detectFace")
 const SVM = require('ml-svm');
+const ProgressBar = require('progress');
+const fs = require("fs");
+//读到文件夹下所有文件的路径
+function findFile(dataPath){
+    let dirArr = []
+    let total = 0
+    let dir = fs.readdirSync(dataPath)
+    dir.map(item=>{
+        let stat = fs.lstatSync(dataPath + item)
+        if (stat.isDirectory() === true) { 
+            let subDirArr = []
+            let subDir = fs.readdirSync(dataPath + item + '/')
+            subDir.map(val=>{
+                let stat = fs.lstatSync(dataPath + item + '/' + val)
+                if(stat.isDirectory() === false){
+                    subDirArr.push(dataPath + item + '/' + val)
+                    total += 1
+                }
+            })
+            dirArr.push(subDirArr)
+        }
+    })
+    return [dirArr, total]
+}
 
-async function SVM(){
+//主文件夹下包含多个子文件夹，每个子文件夹作为一个人脸分类，一个子文件夹包含至少一张人脸图片
+async function loadFiles(dataPath, FacenetModel, Pnet, Rnet, Onet){
+    let [dirArr, total] = findFile(dataPath)
+    const bar = new ProgressBar('  loading :filename Progress :bar :rate/nps :percent :etas', {
+        complete: '█',
+        incomplete: '░',
+        width: 20,
+        total: total
+    });
+    let vectors = []
+    for(let i=0; i<dirArr.length; i++){
+        let subvector = []
+        for(val of dirArr[i]){
+            bar.tick({
+                'filename': val.split("/").pop()
+            });
+            let vector = await facenet.faceAlignVector(FacenetModel, val, Pnet, Rnet, Onet)
+            vector = vector.arraySync()[0]
+            // vector.push(i)
+            // vector = vector.map((val,index)=>{
+            //     return vector.slice(0,index).concat(vector.slice(index+1,vector.length)).concat([index.toString()])
+            // })
+            subvector.push(vector)
+        }
+        vectors.push(subvector)
+    }
+    console.log("loading file over")
+    return vectors
+}
+
+async function SVMClassifier(){
     const trainDatapath = './public/images/RandomForestTrainData1/'
     const predictDatapath = './public/images/RandomForestPredictData/'
     const modelPath = "./public/model/Facenet1/model.json"
@@ -14,17 +67,7 @@ async function SVM(){
     const FacenetModel = await facenet.loadFacenetModel(modelPath)
     const mtcnnModel = await detectFace.loadModel(pModelPath, rModelPath, oModelPath)
 
-    var options = {
-        C: 0.01,
-        tol: 10e-4,
-        maxPasses: 10,
-        maxIterations: 10000,
-        kernel: 'rbf',
-        kernelOptions: {
-            sigma: 0.5
-        }
-    }
-
+    console.log("begin loding ")
     let trainVectors = await loadFiles(trainDatapath, FacenetModel, mtcnnModel[0], mtcnnModel[1], mtcnnModel[2])
     let predictVectors = await loadFiles(predictDatapath, FacenetModel, mtcnnModel[0], mtcnnModel[1], mtcnnModel[2])   
 
@@ -41,4 +84,26 @@ async function SVM(){
     let lowVectorsAll = vectorsAll //采用全部向量
     let lowVectors = vectorsAvg //采用类内平均向量
     
+    let predictions = []
+    for(let i=0; i<trainVectors.length; i++){
+        for(let j=0; j<trainVectors[i].length; j++){
+            predictions.push(i)
+        }
+    }
+    var options = {
+        C: 0.01,
+        tol: 10e-4,
+        maxPasses: 10,
+        maxIterations: 10000,
+        kernel: 'linear',
+        kernelOptions: {
+            sigma: 0.5
+        }
+    }
+    var svm = new SVM(options)
+    console.log(lowVectorsAll)
+    svm.train(lowVectorsAll, predictions)
+    console.log(svm.predict(lowVectorsAll))
 } 
+
+SVMClassifier()
