e76a0d1d5f6f3e7da31a1959daf6a2e1eec065ee
Your Name <you@example.com>
2020-02-26 18:08:01 +0800

    Signed-off-by: 2020.02.26

--- README.md
+++ README.md
@@ -13,3 +13,4 @@ Most of code in ./public/javascripts\<br>
 * 2020.02.21 SVMClassifier.js 编写函数来调用该结构完成对结果的分类
 * 2020.02.24 进行了不同数据集上的测试，继续了解GAN的构建思路。
 * 2020.02.25 GAN.js 进行简单DCGAN的尝试。
+* 2020.02.26 GAN.js DCGAN的generator model和discriminator model的构建。
--- public/javascripts/Gan.js
+++ public/javascripts/Gan.js
@@ -1,56 +1,33 @@
-def build_model(self):
-    if self.y_dim:
-      self.y = tf.placeholder(tf.float32, [self.batch_size, self.y_dim], name='y')
-    else:
-      self.y = None
+const tf = require("@tensorflow/tfjs-node");
+function generator_model(){
+  let model = tf.sequential()
+  model.add(tf.layers.dense({inputDim: 100, units: 1024}))
+  model.add(tf.layers.activation({activation: "tanh"}))
+  model.add(tf.layers.dense({units: 128*7*7}))
+  model.add(tf.layers.batchNormalization())
+  model.add(tf.layers.activation({activation: "tanh"}))
+  model.add(tf.layers.reshape({targetShape: [7, 7, 128], inputShape: [128, 7, 7]}))
+  model.add(tf.layers.upSampling2d({size: [2, 2]}))
+  model.add(tf.layers.conv2d({filters: 64, kernelSize:[5, 5], padding: 'same'}))
+  model.add(tf.layers.activation({activation: "tanh"}))
+  model.add(tf.layers.upSampling2d({size: [2, 2]}))
+  model.add(tf.layers.conv2d({filters: 1, kernelSize:[5, 5], padding: 'same'}))
+  model.add(tf.layers.activation({activation: "tanh"}))
+  return model
+}
 
-    if self.crop:
-      image_dims = [self.output_height, self.output_width, self.c_dim]
-    else:
-      image_dims = [self.input_height, self.input_width, self.c_dim]
-
-    self.inputs = tf.placeholder(
-      tf.float32, [self.batch_size] + image_dims, name='real_images')
-
-    inputs = self.inputs
-
-    self.z = tf.placeholder(
-      tf.float32, [None, self.z_dim], name='z')
-    self.z_sum = histogram_summary("z", self.z)
-
-    self.G                  = self.generator(self.z, self.y)
-    self.D, self.D_logits   = self.discriminator(inputs, self.y, reuse=False)
-    self.sampler            = self.sampler(self.z, self.y)
-    self.D_, self.D_logits_ = self.discriminator(self.G, self.y, reuse=True)
-    
-    self.d_sum = histogram_summary("d", self.D)
-    self.d__sum = histogram_summary("d_", self.D_)
-    self.G_sum = image_summary("G", self.G)
-
-    def sigmoid_cross_entropy_with_logits(x, y):
-      try:
-        return tf.nn.sigmoid_cross_entropy_with_logits(logits=x, labels=y)
-      except:
-        return tf.nn.sigmoid_cross_entropy_with_logits(logits=x, targets=y)
-
-    self.d_loss_real = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(self.D_logits, tf.ones_like(self.D)))
-    self.d_loss_fake = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(self.D_logits_, tf.zeros_like(self.D_)))
-    self.g_loss = tf.reduce_mean(
-      sigmoid_cross_entropy_with_logits(self.D_logits_, tf.ones_like(self.D_)))
-
-    self.d_loss_real_sum = scalar_summary("d_loss_real", self.d_loss_real)
-    self.d_loss_fake_sum = scalar_summary("d_loss_fake", self.d_loss_fake)
-                          
-    self.d_loss = self.d_loss_real + self.d_loss_fake
-
-    self.g_loss_sum = scalar_summary("g_loss", self.g_loss)
-    self.d_loss_sum = scalar_summary("d_loss", self.d_loss)
-
-    t_vars = tf.trainable_variables()
-
-    self.d_vars = [var for var in t_vars if 'd_' in var.name]
-    self.g_vars = [var for var in t_vars if 'g_' in var.name]
-
-    self.saver = tf.train.Saver(max_to_keep=self.max_to_keep)
+function discriminator_model(){
+  let model = tf.sequential()
+  model.add(tf.layers.conv2d({filters: 64, kernelSize:[5, 5], padding: 'same', inputShape: [28, 28, 1]}))
+  model.add(tf.layers.activation({activation: "tanh"}))
+  model.add(tf.layers.maxPooling2d({poolSize: [2, 2]}))
+  model.add(tf.layers.conv2d({filters: 128, kernelSize:[5, 5]}))
+  model.add(tf.layers.activation({activation: "tanh"}))
+  model.add(tf.layers.maxPooling2d({poolSize: [2, 2]}))
+  model.add(tf.layers.flatten())
+  model.add(tf.layers.dense({units: 1024}))
+  model.add(tf.layers.activation({activation: "tanh"}))
+  model.add(tf.layers.dense({units: 1}))
+  model.add(tf.layers.activation({activation: "sigmoid"}))
+  return model
+}
